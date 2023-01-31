5d74793a78fe46b42e9b4e7c9a9c346149201a68
shepard <eviltomorrow@163.com>
2023-01-31 13:18:04 +0800

    dev

--- build/docker_build.sh
+++ build/docker_build.sh
@@ -4,6 +4,6 @@ root_dir=$(pwd)
 app_dir=${root_dir}/app
 for name in $(ls ${app_dir}); do
     echo -e "\033[32m=> Building docker image(${name})...\033[0m"
-    docker build --target prod -t ${name} . --build-arg APPNAME=${name} --build-arg MAINVERSION=${1} --build-arg GITSHA=${2} --build-arg BUILDTIME=${3}
+    docker build --target prod -t eviltomorrow/${name} . --build-arg APPNAME=${name} --build-arg MAINVERSION=${1} --build-arg GITSHA=${2} --build-arg BUILDTIME=${3}
     echo -e "\033[32m=> Build Success\033[0m"
 done
