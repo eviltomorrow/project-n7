e1b45fbe637d790361a7fa283b2b95ccc10c82d3
shepard <eviltomorrow@163.com>
2023-02-02 18:39:12 +0800

    dev

---
+++ app/n7-telegram-bot/handler/telegrambot/accumulator.go
@@ -0,0 +1,8 @@
+package telegrambot
+
+type accumulator struct {
+}
+
+func (a *accumulator) reload() error {
+	return nil
+}
---
+++ app/n7-telegram-bot/handler/telegrambot/bot.go
@@ -0,0 +1,61 @@
+package telegrambot
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/eviltomorrow/project-n7/lib/zlog"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"go.uber.org/zap"
+)
+
+type Bot struct {
+	DomainName  string
+	Pattern     string
+	Port        int
+	AccessToken string
+
+	bot *tgbotapi.BotAPI
+}
+
+func (b *Bot) Run() error {
+	bot, err := tgbotapi.NewBotAPI(b.AccessToken)
+	if err != nil {
+		return err
+	}
+	b.bot = bot
+
+	hook, err := tgbotapi.NewWebhook(fmt.Sprintf("%s%s%s", b.DomainName, b.Pattern, b.AccessToken))
+	if err != nil {
+		return err
+	}
+	if _, err := bot.Request(hook); err != nil {
+		return err
+	}
+
+	update := bot.ListenForWebhook(fmt.Sprintf("%s%s", b.Pattern, b.AccessToken))
+	server := http.Server{
+		Addr: fmt.Sprintf("0.0.0.0:%d", b.Port),
+	}
+	go func() {
+		if err := server.ListenAndServe(); err != nil {
+			zlog.Fatal("ListenAndServe on http failure", zap.Error(err))
+		}
+	}()
+	go func() {
+
+		for u := range update {
+			if u.Message == nil {
+
+			}
+		}
+	}()
+	return nil
+}
+
+func (b *Bot) Stop() error {
+	if b.bot != nil {
+		b.bot.StopReceivingUpdates()
+	}
+	return nil
+}
